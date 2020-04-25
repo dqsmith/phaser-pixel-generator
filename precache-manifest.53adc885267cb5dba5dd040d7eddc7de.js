dd4cd957ecab6f8be7e98c9ef53ea045a257a081
David Smith <davidsmith@MISs-MacBook-Pro.local>
2020-04-24 17:25:21 -0700

    Updates

---
+++ precache-manifest.53adc885267cb5dba5dd040d7eddc7de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6354270a0748b66fa7dc7ccc8a0351",
+    "url": "/phaser-pixel-generator/index.html"
+  },
+  {
+    "revision": "4adc97a95708f37c042f",
+    "url": "/phaser-pixel-generator/static/css/2.3e60228b.chunk.css"
+  },
+  {
+    "revision": "7e1017b95cb60da4e32f",
+    "url": "/phaser-pixel-generator/static/css/main.4dcd24da.chunk.css"
+  },
+  {
+    "revision": "4adc97a95708f37c042f",
+    "url": "/phaser-pixel-generator/static/js/2.51f0f059.chunk.js"
+  },
+  {
+    "revision": "e99720be49daec6db8c42edb2a57b7ae",
+    "url": "/phaser-pixel-generator/static/js/2.51f0f059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1017b95cb60da4e32f",
+    "url": "/phaser-pixel-generator/static/js/main.c71a7552.chunk.js"
+  },
+  {
+    "revision": "b37d9d95831d7dd902a7",
+    "url": "/phaser-pixel-generator/static/js/runtime-main.8d4906bb.js"
+  }
+]);
