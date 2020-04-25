a79c50bc779aab7332fe3326f7ed4458dc5145ac
David Smith <davidsmith@MISs-MacBook-Pro.local>
2020-04-24 17:01:35 -0700

    Add docs dir

---
+++ docs/precache-manifest.8a34fe8dfa89d5371127147c578996f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfbaceb88c641d9b4c8f98751688525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acc024aebcf46fef9aa5",
+    "url": "/static/css/2.3e60228b.chunk.css"
+  },
+  {
+    "revision": "a586e81195060fad439e",
+    "url": "/static/css/main.4dcd24da.chunk.css"
+  },
+  {
+    "revision": "acc024aebcf46fef9aa5",
+    "url": "/static/js/2.3027a3dd.chunk.js"
+  },
+  {
+    "revision": "e99720be49daec6db8c42edb2a57b7ae",
+    "url": "/static/js/2.3027a3dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a586e81195060fad439e",
+    "url": "/static/js/main.3df2fe4b.chunk.js"
+  },
+  {
+    "revision": "790613314c66556ba5ce",
+    "url": "/static/js/runtime-main.090b7b1a.js"
+  }
+]);
